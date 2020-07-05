643c1402116eeaa87ecaf99c079f62611daed5e0
Tarepan <tarepan5884@gmail.com>
2020-07-05 10:47:17 +0000

    Add notification Service worker

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
+// sw load
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").then(
+      (registration) => {
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      },
+      (err) => {
+        // registration failed :(
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
+  });
+}
+
 const btn = document.querySelector("#btn1");
 btn?.addEventListener("click", () => {
   Notification.requestPermission()
