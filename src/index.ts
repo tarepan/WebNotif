fed439d54edb18792c72099842a69e3d6e0fbdc9
Tarepan <tarepan5884@gmail.com>
2020-07-05 11:14:26 +0000

    Fix SW scope

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,22 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     // for GitHub Actions
-    navigator.serviceWorker.register("/WebNotif/sw.js").then(
-      // for local
-      // navigator.serviceWorker.register("/sw.js").then(
-      (registration) => {
-        console.log(
-          "ServiceWorker registration successful with scope: ",
-          registration.scope
-        );
-      },
-      (err) => {
-        // registration failed :(
-        console.log("ServiceWorker registration failed: ", err);
-      }
-    );
+    navigator.serviceWorker
+      .register("/WebNotif/sw.js", { scope: "/WebNotif/" })
+      .then(
+        // for local
+        // navigator.serviceWorker.register("/sw.js").then(
+        (registration) => {
+          console.log(
+            "ServiceWorker registration successful with scope: ",
+            registration.scope
+          );
+        },
+        (err) => {
+          // registration failed :(
+          console.log("ServiceWorker registration failed: ", err);
+        }
+      );
   });
 }
 
