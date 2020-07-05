b543440f29a9fb3549b783ca07d908eb7b040037
Tarepan <tarepan5884@gmail.com>
2020-07-05 09:49:40 +0000

    Add notification

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-import { func1 } from "./func1";
-
-console.log("this is index, hello webDev." as string);
-const x1 = 11;
-
-console.log(`${x1}, ${func1(x1)}`);
+const btn = document.querySelector("#btn1");
+btn?.addEventListener("click", () => {
+  Notification.requestPermission()
+    .then(() => new Promise((resolve) => setTimeout(resolve, 3000)))
+    .then(() => {
+      console.log("notif func called.");
+      new Notification("Hello notif", {
+        body: "this is my original notification.",
+      });
+    });
+});
